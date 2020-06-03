851e8c24f5b58f9faecc5e3651f2904b7db6e2f8
den0206 <mac.pop@mineo.jp>
2020-06-03 21:50:00 +0900

    Initial Commit

---
+++ Swipest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swipest
+//
+//  Created by 酒井ゆうき on 2020/06/03.
+//  Copyright © 2020 Yuuki sakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
